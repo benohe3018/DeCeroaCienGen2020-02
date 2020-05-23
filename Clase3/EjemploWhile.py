1c26266971fd7f85a149ff39bc428831f0ca9ab6
Hayde Martinez <hayde@reverscore.com>
2020-05-23 13:20:28 -0500

    Archivos Clase3

---
+++ Clase3/EjemploWhile.py
@@ -0,0 +1,16 @@
+
+suma = 0
+numero = 1
+
+while numero != 0:
+    suma = numero + suma
+    numero = numero + 1
+print ("La suma es " + str(suma))
+
+print("¿Cómo se llama?")
+nombre = input()
+print("Me alegro de conocerle "+ str(nombre))
+
+def contar(tupla, entero):
+    #escribo mi codigo
+    return cuantasvecesserepiteelentero
--- Clase3/recursividad.py
+++ Clase3/recursividad.py
@@ -1,15 +1,15 @@
-def jugar(intento=1):
-    respuesta = input("¿De qué color es una naranja? ")
-    if respuesta != "naranja":
-        if intento < 3:
-            print ("\nFallaste! Inténtalo de nuevo")
-            intento += 1
-            jugar(intento)
-        else:
-            print("\nPerdiste!")
-    else:
-        print("\nGanaste!")
-jugar()
+# def jugar(intento=1):
+#     respuesta = input("¿De qué color es una naranja? ")
+#     if respuesta != "naranja":
+#         if intento < 3:
+#             print ("\nFallaste! Inténtalo de nuevo")
+#             intento += 1
+#             jugar(intento)
+#         else:
+#             print("\nPerdiste!")
+#     else:
+#         print("\nGanaste!")
+# jugar()
 
 def calc_factorial(x):
     if x == 1:
